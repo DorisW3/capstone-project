32ffef16313133308dd8f817babe14e4c0ab3c19
Doris Wiebe <doris-wiebe@gmx.de>
2023-08-15 16:44:29 +0200

    add key-value pair: usernamein db.js

--- db.js
+++ db.js
@@ -4,18 +4,21 @@ const pictures = [
     image: "/AliceInWonderland.png",
     theme: "Alice in wonderland",
     description: 'a collage with some characters of "alice in wonderland" ',
+    username: "dolorisi",
   },
   {
     id: 2,
     image: "/Apple.png",
     theme: "Apple",
     description: "gouache artwork of an apple painted in Procreate app",
+    username: "dolorisi",
   },
   {
     id: 3,
     image: "/Segelboote.png",
     theme: "Sailboats on the sea",
     description: "acryl artwork of sailboats on the sea",
+    username: "dolorisi",
   },
   {
     id: 4,
@@ -23,6 +26,7 @@ const pictures = [
     theme: "Wickie und die starken Männer",
     description:
       "Acryl painting on a huge cardboard, like a cardboard stand up",
+    username: "dolorisi",
   },
   {
     id: 5,
@@ -30,6 +34,7 @@ const pictures = [
     theme: "Die Stadt der Träumenden Bücher",
     description:
       'Sketch of some characters from the book "Die Stadt der Träumenden Bücher"',
+    username: "dolorisi",
   },
 ];
 
