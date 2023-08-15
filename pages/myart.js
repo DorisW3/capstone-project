6a245434daf9b89384776a6fe7cdcec5371c08a2
Doris Wiebe <doris-wiebe@gmx.de>
2023-08-15 21:15:21 +0200

    change name of homepage.js into myart.js

---
+++ pages/myart.js
@@ -0,0 +1,27 @@
+import { Fragment } from "react";
+import { styled } from "styled-components";
+import PictureList from "../components/PictureList";
+import pictures from "../db.js";
+
+export default function MyArt() {
+  return (
+    <>
+      <Heading>My Art</Heading>
+      <StyledMain>
+        <PictureList pictures={pictures} />
+      </StyledMain>
+    </>
+  );
+}
+
+const Heading = styled.h1`
+  text-align: center;
+  font-size: 2rem;
+  margin-bottom: 2rem;
+`;
+
+const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
